ca81c4079de3ac736b52d34bc18d8821bdad2e14
David Acosta <davidacosta25081@yahoo.com>
2019-11-05 21:42:16 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -1,9 +1,9 @@
 class SightingSerializer
 
   def initialize (sighting_object)
-    @sighting = sighting_object 
-  end 
-    
+    @sighting = sighting_object
+  end
+
 
   def to_serialized_json
     @sighting.to_json(:include => {
